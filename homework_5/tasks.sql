f01205044c21e2fc7a41db2ccdd31366a10e02a4
leshark <al.pyrko@yandex.ru>
2019-08-31 16:13:57 +0300

    add second task

--- homework_5/tasks.sql
+++ homework_5/tasks.sql
@@ -23,7 +23,8 @@ SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY FIELD(id, 5, 1, 2);
 SELECT AVG(age) FROM (SELECT YEAR(CURRENT_TIMESTAMP) - YEAR(birthday) as age FROM profiles) AS Avg_age;
 
 -- 2) Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. Следует учесть, что необходимы дни недели текущего года, а не года рождения
-
+SELECT COUNT(*) as stats from (SELECT DAYOFWEEK(CONCAT(YEAR(NOW()),'-',MONTH(birthday),'-',DAYOFMONTH(birthday))) as date from profiles) as stats where date=1;
+-- где date=1 для понедельника, date=2 для вторника и т.д
 
 -- 3) Подсчитайте произведение чисел в столбце таблицы
 SELECT EXP(sum(log(value))) from table;
